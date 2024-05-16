9d9e10d6a8a071f01492226032410dfc6db76d9d
mathildepfx <mathilde.portefaix@edu.ece.fr>
2024-05-17 00:11:35 +0200

    fichier mon profil

---
+++ monProfil.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang= "fr">
+<html>
+
+<?php
+require_once("config.php");
+?>
+
+<head>
+<title > BlablaOMNES.com </title >
+<meta charset= " UTF-8 ">
+<link rel= " stylesheet " href= "designCreationCompte.css">
+<div class= " feetpage ">Mon profil</div ><br>
+</head>
+
+<body>
+<form method="post" action="AjoutCompte.php">
+        <button type="submit">Mes trajets</button>
+</form>
+
+<p><a href="verifMonProfil.php?"></p>
+</body>
+</html>
