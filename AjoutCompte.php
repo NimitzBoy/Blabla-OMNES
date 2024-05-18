549ec49d86709060802a166da889ad2c708b5d5e
mathildepfx <mathilde.portefaix@edu.ece.fr>
2024-05-18 17:57:53 +0200

    ajoutCompte

--- AjoutCompte.php
+++ AjoutCompte.php
@@ -2,17 +2,17 @@
 require_once("config.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["adresseMail"]; 
-    $motDePasse = $_POST["motDePasse"];
+    $motDePasse = password_hash($_POST["motDePasse"], PASSWORD_DEFAULT);
     $nom = $_POST["nom"]; 
     $prenom = $_POST["prenom"];
-    //$numero = $_POST["Numero_de_telephone"];
+    $numero = $_POST["Numero_de_telephone"];
     try {
-        $stmt = $bdd->prepare("INSERT INTO Utilisateur (email, mot_de_passe,nom,prenom) VALUES (:email, :motDePasse, :nom, :prenom)");
+        $stmt = $bdd->prepare("INSERT INTO Utilisateur (email, mot_de_passe,nom,prenom,numero) VALUES (:email, :motDePasse, :nom, :prenom, :numero)");
         $stmt->bindParam(':email', $email);
         $stmt->bindParam(':motDePasse', $motDePasse);
         $stmt->bindParam(':nom', $nom);
         $stmt->bindParam(':prenom', $prenom);
-        //$stmt->bindParam(':numero', $numero);
+        $stmt->bindParam(':numero', $numero);
         if ($stmt->execute()) {
             echo "Données ajoutées avec succès.";
         } else {
